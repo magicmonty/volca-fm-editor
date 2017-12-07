f3dceeb54928521066cea9fec1ff6515102f5e8e
Martin Gondermann <magicmonty@pagansoft.de>
2017-12-07 21:53:37 +0100

    VS Code tasks for build, run and release

--- RELEASE_NOTES.md
+++ RELEASE_NOTES.md
@@ -1,6 +1,7 @@
 ### 0.0.2 - 2017-12-07
 * Cleanup, Renaming
 * better handling of MIDI events
+* VS Code tasks for build, run and release
 
 ### 0.0.1 - 2017-12-06
 * Initial release
--- src/Client/ReleaseNotes.fs
+++ src/Client/ReleaseNotes.fs
@@ -1,10 +1,14 @@
 module internal ReleaseNotes
 
-let Version = "0.0.1"
+let Version = "0.0.2"
 
 let IsPrerelease = false
 
 let Notes = """
-### 0.0.1 - 2017-11-24
+### 0.0.2 - 2017-12-07
+* Cleanup, Renaming
+* better handling of MIDI events
+
+### 0.0.1 - 2017-12-06
 * Initial release
 """
