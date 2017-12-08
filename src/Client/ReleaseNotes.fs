7248a4e61412e745630525cd518a4a88f63184fb
Martin Gondermann <mgondermann@np4.net>
2017-12-08 09:56:48 +0100

    Bumping version to 0.0.4

--- src/Client/ReleaseNotes.fs
+++ src/Client/ReleaseNotes.fs
@@ -1,10 +1,14 @@
 module internal ReleaseNotes
 
-let Version = "0.0.3"
+let Version = "0.0.4"
 
 let IsPrerelease = false
 
 let Notes = """
+### 0.0.4 - 2017-12-08
+* Fix Margins on cards for better responsive support
+* badge-pill
+
 ### 0.0.3 - 2017-12-08
 * Fix accidentally selection on slider movement
 
